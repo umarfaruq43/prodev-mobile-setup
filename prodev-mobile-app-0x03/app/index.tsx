bc9505269beaa38f372be654be0d130f24f732a1
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-11-02 18:15:02 +0100

    Explore More Core Components

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -1,7 +1,9 @@
-import { Text, TextInput, View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
 
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { styles } from "@/styles";
+
 
 export default function Index() {
   return (
@@ -92,120 +94,3 @@ export default function Index() {
 }
 
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    position: 'relative'
-  },
-  navGroup: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-    marginTop: 11
-  },
-  largeText: {
-    fontSize: 40,
-    fontWeight: 700,
-  },
-  smallText: {
-    fontSize: 12,
-    color: '#7E7B7B'
-  },
-  placeholderText: {
-    fontSize: 18,
-    color: '#7E7B7B',
-    marginBottom: 7
-  },
-  inputField: {
-    borderWidth: 1,
-    borderRadius: 10,
-    height: 50,
-    borderColor: '#7E7B7B',
-    paddingHorizontal: 10
-  },
-  passwordGroup: {
-    flexDirection: 'row',
-    borderWidth: 1,
-    alignItems: 'center',
-    height: 50,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    borderColor: '#7e7b7b'
-  },
-  formGroup: {
-    marginTop: 44
-  },
-  forgotPasswordText: {
-    fontSize: 17,
-    marginTop: 9,
-    textAlign: 'right',
-    color: '#34967C'
-  },
-  button: {
-    backgroundColor: '#2B876E',
-    height: 53,
-    borderRadius: 10,
-    marginTop: 25,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  buttonText: {
-    fontSize: 17,
-    color: 'white'
-  },
-  socialMediaButton: {
-    borderWidth: 1,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10
-  },
-  socialMediaButtonText: {
-    fontSize: 18,
-    color: '#0D0D0D',
-    fontWeight: 400
-  },
-  socialMediaButtonGroup: {
-    gap: 15,
-    marginTop: 25
-  },
-  dividerGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 10,
-    marginTop: 29
-  },
-  divider: {
-    borderWidth: 0.5,
-    borderColor: "#c2c2c2",
-    flex: 1
-  },
-  dividerText: {
-    fontSize: 17,
-    color: "#C2C2C2",
-  },
-  subTextGroup: {
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 33,
-    left: 77,
-    right: 76
-  },
-  subText: {
-    fontSize: 18,
-    color: '#b5b5b5'
-  },
-  subTextJoin: {
-    fontSize: 18,
-    color: '#FFA800',
-    fontWeight: 600
-  }
-});
-
-
-export {
-  styles
-}
